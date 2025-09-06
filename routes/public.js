d4891c4eaa11a01b010b9899b7e97d167f5d0ab8
SisterKrone <luiz.fabiana12@gmail.com>
2025-09-06 18:59:05 -0300

    Router .post

--- routes/public.js
+++ routes/public.js
@@ -7,22 +7,26 @@ const router = express.Router();
 
 
 router.post('/cadastro', async (req, res) => {
-    const user = req.body;
-    const salt =
-
     try {
-        await prisma.user.create({
+        const user = req.body;
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(user.password, salt)
+
+        const userDB = await prisma.user.create({
             data: {
                 email: user.email,
                 name: user.name,
-                password
+                password: hashPassword,
             }
-        })
-        res.status(201).json(user);
+        });
+        res.status(201).json(userDB);
     }
     catch (err) {
-        res.status(500).json({ message: "Erro no servidor, tente novamente." })
+        res.status(500).json({ message: "Erro no servidor, tente novamente." });
     }
 });
+
+
+
 
 export default router;
