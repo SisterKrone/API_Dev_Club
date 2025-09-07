1cc82a749582733e36e0a392562ac2931206b39d
SisterKrone <luiz.fabiana12@gmail.com>
2025-09-07 07:41:44 -0300

    feat: login.post

--- routes/public.js
+++ routes/public.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import { PrismaClient } from '@prisma/client';
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 const prisma = new PrismaClient();
 const router = express.Router();
+
+const JWT_SECRET = process.env.JWT_SECRET
 
 
 router.post('/cadastro', async (req, res) => {
@@ -26,7 +29,32 @@ router.post('/cadastro', async (req, res) => {
     }
 });
 
+router.post('/login', async (req, res) => {
+    try {
+        const userInfo = req.body;
+        const user = await prisma.user.findUnique({ where: { email: userInfo.email } });
 
+        if (!user) {
+            return res.status(404).json({ message: "Usuário não encontrado." });
+        }
+
+        const userIsMatch = await bcrypt.compare(userInfo.password, user.password);
+
+        if (!userIsMatch) {
+            return res.status(400).json({ message: "Senha incorreta." });
+        }
+
+
+        const token = jwt.sign({id: user.id}, JWT_SECRET, {expiresIn: '1m'})
+
+
+        res.status(200).json(token)
+        
+    }
+    catch (err) {
+        res.status(500).json({ message: "Erro no servidor, tente novamente." });
+    }
+});
 
 
 export default router;
