6c4caab25ed2e11999f10f4f85984bd38dc0a80b
SisterKrone <luiz.fabiana12@gmail.com>
2025-09-08 23:35:37 -0300

    gitignore adjust

--- routes/public.js
+++ routes/public.js
@@ -45,11 +45,11 @@ router.post('/login', async (req, res) => {
         }
 
 
-        const token = jwt.sign({id: user.id}, JWT_SECRET, {expiresIn: '1m'})
+        const token = jwt.sign({ id: user.id }, JWT_SECRET, { expiresIn: '7d' })
 
 
         res.status(200).json(token)
-        
+
     }
     catch (err) {
         res.status(500).json({ message: "Erro no servidor, tente novamente." });
